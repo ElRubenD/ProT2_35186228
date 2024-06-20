edb7d628f356a2dcf828d351a473f848bbf48b53
ElRubenD <ruben.d.ledesma@gmail.com>
2024-06-20 13:22:48 -0300

    cambios en el Footer y en los formularios

--- app/Views/front/login.php
+++ app/Views/front/login.php
@@ -1,13 +1,15 @@
-<section>
-  <div class="container col-8">
-          <h2>Inicio de Sesión</h2>
-          <form>
+<section class="formulario">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-6 col-lg-5">
+                <h2 class="text-center mb-4">Inicio de Sesión</h2>
+                <form>
               <div class="form-group mb-3">
-                  <label for="username">Email</label>
+                  <label for="username" class="row content-left derecha">Email</label>
                   <input type="text" class="form-control" id="username" placeholder="Ingresa tu email">
               </div>
               <div class="form-group mb-3">
-                  <label for="password">Contraseña</label>
+                  <label for="password" class="row content-left derecha">Contraseña</label>
                   <input type="password" class="form-control" id="password" placeholder="Ingresa tu contraseña">
               </div>
               <div class="form-group">
@@ -15,8 +17,10 @@
                   <button type="reset" class="btn btn-danger">Cancelar</button>
               </div>
               <div class="form-group">
-                  <p class="fp">¿Aún no se ha registrado? <a href="registro">Regístrese AQUÍ</a></p>
+                  <p class="fp">¿Aún no te has registrado? <a href="registro">Regístrese AQUÍ</a></p>
               </div>
           </form>
-      </div>
+            </div>
+        </div>
+    </div>
 </section>
